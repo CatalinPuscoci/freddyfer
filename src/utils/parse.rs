dd20b0e6c02affd38c08391f0341a619b1757082
Catalin Puscoci <catalinpuscoci@protonmail.com>
2023-03-13 23:32:16 +0200

    added sound not found response

--- src/utils/parse.rs
+++ src/utils/parse.rs
@@ -1,4 +1,6 @@
+use rand::seq::SliceRandom;
 use serenity::framework::standard::Args;
+use std::path::Path;
 
 pub(crate) fn get_repeat_count(mut args: Args, fallback_value: i32) -> i32 {
     match args.single::<i32>() {
@@ -14,5 +16,13 @@ pub(crate) fn get_repeat_count(mut args: Args, fallback_value: i32) -> i32 {
 }
 
 pub(crate) fn get_sound_path(sound_name: &str) -> String {
-    format!("sounds/{}", sound_name)
+    let path = format!("sounds/{}", sound_name);
+    if Path::new(path.as_str()).exists() {
+        return path
+    }
+
+    else {
+        let sounds = vec!["sounds/ilie_cum.mp3","sounds/ilie_ha.mp3"];
+        return sounds.choose(&mut rand::thread_rng()).unwrap().to_string();
+    }
 }
